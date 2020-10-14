23b8d077a8daf267b87d66f64341068b18e1f395
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2020-10-15 00:30:56 +0300

    Basic diffuse

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,27 @@ use std::fs::File;
 use std::io::{ BufWriter, Write };
 use rand::Rng;
 
+// TODO: implement and benchmark a different point picking algo
+fn rnd_in_sphere() -> Vec3 {
+    let mut rng = rand::thread_rng();
+    let mut result;
+    let offset = Vec3::new_all(1.0);
+
+    loop {
+        result = Vec3::new(rng.gen(), rng.gen(), rng.gen()) * 2.0 - offset;
+        if result.len_sq() < 1.0 {
+            break;
+        }
+    }
+
+    result
+}
 
 fn color(ray: &Ray, world:&HitList) -> Vec3 {
     let mut rec = HitRecord::new();
-    if world.hit(&ray, 0.0, f32::MAX, &mut rec) {
-        return Vec3::new(rec.normal.x() + 1.0,
-            rec.normal.y() + 1.0,
-            rec.normal.z() + 1.0) * 0.5
+    if world.hit(&ray, 0.001, f32::MAX, &mut rec) {
+        let target = rec.p + rec.normal + rnd_in_sphere();
+        return color(&Ray::new(rec.p, target - rec.p), &world) * 0.5;
     }
 
     let dir = ray.direction().into_unit();
@@ -37,7 +51,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let width = 200;
     let height = 100;
-    let aa_rays = 100;
+    let aa_rays = 20;
     let camera = Camera::new();
 
     let mut rng = rand::thread_rng();
@@ -59,11 +73,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
             // TODO: implement DivAssign for Vec3
             col = col / aa_rays as f32;
+
+            col = Vec3::new(col.r().sqrt(),
+                col.g().sqrt(),
+                col.b().sqrt());
             
             write!(&mut out, "{} {} {}\n",
-                (col.x() * 255.9) as u8,
-                (col.y() * 255.9) as u8,
-                (col.z() * 255.9) as u8)?;
+                (col.r() * 255.99) as u8,
+                (col.g() * 255.99) as u8,
+                (col.b() * 255.99) as u8)?;
         }
     }
 
