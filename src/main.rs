6910aef5a6f86d3906bf9ee3d75e9fc6e35ecb46
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2020-10-14 17:37:22 +0300

    Hello normal

--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,26 @@ use ray::Ray;
 use std::fs::File;
 use std::io::{ BufWriter, Write };
 
-fn hit_sphere(center: Vec3, radius: f32, ray:&Ray) -> bool {
+fn hit_sphere(center: Vec3, radius: f32, ray:&Ray) -> f32 {
     let diff = ray.origin() - center;
     let a = ray.direction().dot(&ray.direction());
     let b = diff.dot(&ray.direction()) * 2.0;
     let c = diff.dot(&diff) - radius * radius;
     let d = b * b - 4.0 * a * c;
-    d > 0.0
+    if d < 0.0 {
+        -1.0
+    } else {
+        (-b - d.sqrt()) / 2.0 / a
+    }
 }
 
 fn color(ray: &Ray) -> Vec3 {
-    if hit_sphere(Vec3::new(0.0,0.0,-1.0), 0.5, ray) {
-        return Vec3::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(Vec3::new(0.0,0.0,-1.0), 0.5, ray);
+    if t > 0.0 {
+        let n = (ray.point_at(t) - Vec3::new(0.0, 0.0, -1.0)).into_unit();
+        return Vec3::new(n.x() + 1.0, n.y() + 1.0, n.z() + 1.0) * 0.5;
     }
+
     let dir = ray.direction().into_unit();
     let t = 0.5 * (dir.y() + 1.0);
     Vec3::new_all(1.0) * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t
